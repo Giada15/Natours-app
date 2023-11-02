e4e2ceb705ad6a0bfa251bf36110a639dbc20be7
Giada15 <tascheriogiada@gmail.com>
2023-11-02 10:20:08 -0400

    Set Node.js enviroment variables
    In this commit: -
    -Introduced a 'config.env' file to configure the 'NODE_ENV' variable as 'development'.
    -Added a production script to set the 'NODE_ENV' variable to 'production'.
    - Utilized the 'morgan' package based on the 'NODE_ENV' variable's value.

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const userRouter = require('./routes/userRoute');
 const app = express();
 
 // 1) Middlewares
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
